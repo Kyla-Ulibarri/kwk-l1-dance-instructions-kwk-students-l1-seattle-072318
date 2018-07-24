97b7794f43ee0eeca3f800d3a4b922734c1ba3bd
Kyla Ulibarri <k.barri.071@gmail.com>
2018-07-24 19:52:07 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,15 @@
 
 # Code your methods below
+def lower_body
+  puts "Body Stance: plant legs far apart, bend knees slightly and keep posture loose"
+  puts "Footwork: lift right foot, return right foot to the ground, finish with a small skip-step backward"
+end
+
+def skip_step
+  puts "Skip Step Left:lower left foot to the ground; bounce left foot back up slightly, kicking it a few inches back; left right, left and left"
+  puts "Skip Step Right: lower right foot to the ground; bounce right foot back up slightly, kicking it a few inches back; right, left, right and right"
+end
+def upper_body
+  puts "Bob the Reins: holding your arms out in front of you, straight and at chest level; cross your right wrist over your left and hold them together; lift your arms up and down in a  loose bouncing movement, in beat wtih the time of the song eight times"
+  puts "Lasso: start arm movement 
+end
